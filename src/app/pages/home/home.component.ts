f968ce55cc952890eea4a4dd0749a346cd85e8ba
Evelyn Pulgarin Gomez <evelynggomez9080@gmail.com>
2025-03-24 22:53:53 -0500

    user.services

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CardComponent } from '../../componets/card/card.component';
+import { UsersService } from '../../services/users.service';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export  default class HomeComponent {
 
+  users:any = [];
+  UsersService = inject(UsersService);
+
+  ngOnInit(){
+     this.UsersService.getUsers().then((data) => { 
+      this.users = data;
+     })
+  }
 }
